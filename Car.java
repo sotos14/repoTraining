38961c8eac81c5ac9c8388d22733a76659092b0c
Sotirios Banatas <sbanatas@financialforce.com>
2014-09-12 16:00:04 +0100

    ADD methods to the car class

--- Car.java
+++ Car.java
@@ -1,3 +1,52 @@
 public class Car{
 	
+	String manufacturer;
+	String color;
+	String type;
+	int engineSize;
+
+	public Car(String manufacturer)
+	{
+		this.manufacturer = manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer)
+	{
+		this.manufacturer = manufacturer;
+	}
+
+	public String getManufacturer()
+	{
+		return manufacturer;
+	}	
+
+	public void setColor(String color)
+	{
+		this.color = color;
+	}
+
+	public String getColor()
+	{
+		return color;
+	}
+
+	public void setType(String type)
+	{
+		this.type = type;
+	}
+
+	public String getType()
+	{
+		return type;
+	}
+
+	public void setEngineSize(int engineSize)
+	{
+		this.engineSize = engineSize;
+	}
+
+	public int getEngineSize()
+	{
+		return engineSize;
+	}
 }
