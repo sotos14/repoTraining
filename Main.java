870b266d5eb416be09d0ab920409a7586bdf0a92
Sotirios Banatas <sbanatas@financialforce.com>
2014-09-12 17:01:29 +0100

    changes to main

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
 public class Main{
 	public static void main(String[] args){
+
+		Driver driver = new Driver("Sotos");
+
+		Car car = new Car("BMW", "Black", "Sedan", 2.0);
+
+		driver.addCar(car);
+
+		System.out.println(driver.getInfo());
 	
 	}
 }
