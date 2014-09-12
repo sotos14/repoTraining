1fe1a40e9f9ff5d28bcf89c8ecf065a6d5347035
Sotirios Banatas <sbanatas@financialforce.com>
2014-09-12 15:49:43 +0100

    ADD methods for the driver class

--- Driver.java
+++ Driver.java
@@ -1,3 +1,31 @@
 public class Driver{
 	
+	private String driverName;
+	private String licenceType
+	private int age;
+	
+	public Driver(String driverName)
+	{
+		this.driverName = driverName;
+	}
+
+	public String getName()
+	{
+		return driverName;
+	}
+
+	public void setAge(int age)
+	{
+		this.age = age;
+	}
+
+	public int getAge()
+	{
+		return age;
+	}
+
+	public void setLicenceType(String licenceType)
+	{
+		this.licenceType = licenceType;
+	}
 }
